4ca4cd10e71d8d98033c25dbc135b1cdaafabab4
vitali-f <lonely.cool23@gmail.com>
2026-02-24 16:31:32 +0300

    dim

--- models/src/src_hosts.sql
+++ models/src/src_hosts.sql
@@ -1,12 +1,18 @@
 WITH raw_hosts AS (
+    /* Задание 2: Используем CTE 'raw_hosts'. 
+    Обращаемся к источнику через функцию source.
+    */
     SELECT
         *
     FROM
         {{ source('airbnb', 'hosts') }}
 )
+
+/* Задание 3: Финальный SELECT с переименованием колонок
+*/
 SELECT
-    id AS host_id,
-    name AS host_name,
+    id AS host_id,       -- Переименовываем id в host_id
+    name AS host_name,   -- Переименовываем name в host_name
     is_superhost,
     created_at,
     updated_at
